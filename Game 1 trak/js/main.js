36ebdac5c12a3f1421e2796ea111c7e8c48785be
Taylor Rak <taylorrak@gmail.com>
2018-02-08 01:18:31 -0500

    Fin??

--- Game 1 trak/js/main.js
+++ Game 1 trak/js/main.js
@@ -10,40 +10,56 @@ window.onload = function() {
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, onTap: onTap } );
     
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.image( 'wall', 'assets/cave_wall.jpg' );
+        game.load.image( 'cave', 'assets/crystal_cave.jpg' );
+        game.load.audio('sfx', 'assets/pickaxe_sfx.wav');
+        game.load.audio('fall', 'assets/rockfall.mp3');
     }
     
-    var bouncy;
+    var fx;
+    let x = 0;
+    var fall;
+    var style = { font: "50px Impact", fill: "#FF0000", align: "center" };
     
     function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
         
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
+        var text = game.add.text( game.world.centerX, 15, "!!!BREAK THROUGH!!!", style );
         text.anchor.setTo( 0.5, 0.0 );
+
+        game.add.image(0, 0, 'wall');
+
+        //read mouse input
+       game.input.onTap.add(onTap, this);
+        
+        //creating sound markers
+        fx = game.add.audio('sfx');
+        fall = game.add.audio('fall');
+        fx.allowMultiple = true;
+        fx.addMarker('hit1', 1, 1.0);
+       /* fx.addMarker('hit2', 2, 1.0);
+        fx.addMarker('hit3', 3, 1.0);
+        fx.addMarker('hit4', 4, 1.0);
+        fx.addMarker('hit5', 5, 1.0);
+        fx.addMarker('hit6', 6, 1.0);*/
+
     }
     
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
-    }
+  function onTap(pointer, doubleTap) {
+     if(x<7){
+         fx.play("hit1");
+         x++;
+     }
+     else{
+         fall.play();
+         game.add.image(0,0,'cave');
+         var text = game.add.text( game.world.centerX, 15, "!!!BIG BREAK!!!", style );
+     }
+   }
+
 };
